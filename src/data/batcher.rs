b0e5e42e791d97f6c4b58427eea09c6f46bb8ff6
ethqnol <112029223+ethqnol@users.noreply.github.com>
2025-01-27 12:12:42 -0500

    batcher + data parser

---
+++ src/data/batcher.rs
@@ -0,0 +1,64 @@
+#![allow(unused_variables, dead_code, unused_imports)]
+
+use crate::data::sgf_parser::*;
+use burn::{data::dataloader::batcher::Batcher, prelude::*};
+
+#[derive(Clone, derive_new::new)]
+pub struct GoBatcher<B: Backend> {
+    device: B::Device,
+}
+
+#[derive(Clone, Debug)]
+pub struct GoItem {
+    pub board: Vec<u8>,
+}
+
+#[derive(Clone, Debug)]
+pub struct GoBatch<B: Backend> {
+    pub board_states: Tensor<B, 4, Int>,
+    pub targets: Tensor<B, 1, Int>,
+}
+
+impl<B: Backend> Batcher<GoItem, GoBatch<B>> for GoBatcher<B> {
+    fn batch(&self, items: Vec<GoItem>) -> GoBatch<B> {
+        let (board_states, targets): (Vec<Vec<u8>>, Vec<usize>) = items
+            .iter()
+            .filter_map(|item| {
+                let parser_result = sgf_parser(&item.board);
+                match parser_result {
+                    Ok((board_state, expected_move)) => return Some((board_state, expected_move)),
+                    Err(_) => None,
+                }
+            })
+            .collect();
+
+        let board_states: Tensor<B, 4, Int> = Tensor::stack(
+            board_states
+                .iter()
+                .map(|item| TensorData::new(item.clone(), Shape::new([9, 9, 1])))
+                .map(|tensor_data| {
+                    Tensor::<B, 3, Int>::from_data(tensor_data, &self.device).permute([2, 0, 1])
+                })
+                .collect(),
+            0,
+        );
+
+        let targets = Tensor::cat(
+            targets
+                .iter()
+                .map(|item| {
+                    Tensor::<B, 1, Int>::from_data(
+                        TensorData::from([(item.clone() as i64).elem::<B::IntElem>()]),
+                        &self.device,
+                    )
+                })
+                .collect(),
+            0,
+        );
+
+        return GoBatch {
+            board_states,
+            targets,
+        };
+    }
+}
---
+++ src/data/sgf_parser.rs
@@ -0,0 +1,63 @@
+#![allow(dead_code, unused_imports, unused_variables)]
+
+use std::error::Error;
+
+static BOARD_SIZE: usize = 9;
+
+pub struct ParseError {
+    note: String,
+}
+
+pub fn sgf_parser(sgf_ascii: &Vec<u8>) -> Result<(Vec<u8>, usize), ParseError> {
+    let sgf = std::str::from_utf8(&sgf_ascii[5..BOARD_SIZE]).expect("invalid utf-8 sequence");
+    let mut board = vec![0u8; BOARD_SIZE * BOARD_SIZE];
+
+    let mut current_player = 1;
+
+    let moves = parse_moves(sgf);
+
+    for i in 0..moves.len() - 1 {
+        let (x, y) = moves[i];
+        if x < BOARD_SIZE && y < BOARD_SIZE {
+            board[encode_to_1d((x, y))] = current_player;
+            current_player = if current_player == 1 { 2 } else { 1 };
+        } else {
+            return Err(ParseError {
+                note: "Poorly formatted board".to_string(),
+            });
+        }
+    }
+    let last_move = moves[moves.len() - 1];
+    if last_move.0 < BOARD_SIZE || last_move.1 < BOARD_SIZE {
+        return Err(ParseError {
+            note: "Poorly formatted board".to_string(),
+        });
+    }
+    return Ok((board, encode_to_1d(last_move)));
+}
+
+fn parse_moves(sgf: &str) -> Vec<(usize, usize)> {
+    let mut moves = Vec::new();
+
+    for token in sgf.split(';') {
+        if token.starts_with("B[") || token.starts_with("W[") {
+            if let Some(coords) = token.get(2..4) {
+                if coords.len() == 2 {
+                    let x = (coords.chars().nth(0).unwrap() as usize) - ('a' as usize);
+                    let y = (coords.chars().nth(1).unwrap() as usize) - ('a' as usize);
+                    moves.push((x, y));
+                }
+            }
+        }
+    }
+
+    moves
+}
+
+pub fn encode_to_1d(loc: (usize, usize)) -> usize {
+    return loc.0 * BOARD_SIZE + loc.1;
+}
+
+pub fn encode_to_2d(loc: usize) -> (usize, usize) {
+    return (loc % BOARD_SIZE, loc / BOARD_SIZE);
+}
